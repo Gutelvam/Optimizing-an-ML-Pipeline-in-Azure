66c6c8115fc62fda4aba590eef1cbc7191f14f68
Gutelvam Rodrigues <56238851+Gutelvam@users.noreply.github.com>
2021-02-19 03:29:14 +0000

    feat: Add data reader and run pipeline
    This commit intend to create data reader and fix return from the function.
    Train_test = 70/30  acc: 0.9150227617602428

--- train.py
+++ train.py
@@ -14,15 +14,7 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+ds = TabularDatasetFactory.from_delimited_files(['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'])
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,8 +41,17 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
 
+    return x_df,y_df
+
+## make categorical class numerical and remove NaN values  
+x, y = clean_data(ds)
+
+## information from docummentation: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+x_train, x_test, y_train, y_test = train_test_split(x , y , test_size = .25, random_state= 0)
+run = Run.get_context()
+
+  
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
